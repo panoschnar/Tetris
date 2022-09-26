da69f8d678d2a766e68f85c1c82cd93c471ab510
Panagiotis Chnarakis <panagiotis.chnarakis@Peoplecert.onmicrosoft.com>
2022-09-26 20:45:32 +0300

    Add project files.

---
+++ Tetris/Block.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffSet { get; }
+
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+        public Block()
+        {
+            offset = new Position(StartOffSet.Row, StartOffSet.Column);
+        }
+
+        public IEnumerable<Position> TilePosition()
+        {
+            foreach(Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row,p.Column + offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if(rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row=StartOffSet.Row;
+            offset.Column=StartOffSet.Column;
+        }
+
+
+
+    }
+}
---
+++ Tetris/OBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class OBlock : Block
+    {
+        public override int Id => 4;
+
+        protected override Position[][] Tiles => tiles;
+
+        protected override Position StartOffSet => new Position(0, 4);
+
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[]{new(0,0), new(0, 1), new(1, 0), new(1,1)}
+        };
+
+    }
+}
